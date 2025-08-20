3c587457a34c560d6c4e37c4e8c5383d3ccb0021
sahilsukhani <sahilsukhani29@gmail.com>
2025-08-20 11:46:57 +0530

    Update CORS configuration to use the full production URL for frontend requests

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "viewlist.vercel.app",
+    origin: "https://viewlist.vercel.app",
     credentials: true,
   })
 );
